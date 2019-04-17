55c6c381809bf697fe799bc9f0f7a9d774c7c34a
272373810@qq.com <272373810@qq.com>
2019-04-17 22:28:02 +0800

    api address edit

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -10,7 +10,7 @@ import store from '../store'
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers['Authentication'] = localStorage.getItem('token');
 let config = {
-  baseURL: 'https://c.api.msspay.com/',
+  baseURL: 'https://api.sipinoffice.com/',
   timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
 };
